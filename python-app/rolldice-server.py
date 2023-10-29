cab83cbbd8ab15e4f64824011aeeb88d2b447462
Alex Boten <alex@boten.ca>
2023-10-29 07:29:14 -0700

    add python server app
    Signed-off-by: Alex Boten <alex@boten.ca>

---
+++ python-app/rolldice-server.py
@@ -0,0 +1,13 @@
+from random import randint
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/rolldice")
+def roll_dice():
+    return str(roll())
+
+def roll():
+    return randint(1, 6)
+
+app.run()
