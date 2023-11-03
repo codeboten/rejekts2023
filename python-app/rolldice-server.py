ad57f4d4d14d5b55aacf169ce14205d7af053cb7
Alex Boten <alex@boten.ca>
2023-11-03 15:56:43 -0700

    add config + docker file
    Signed-off-by: Alex Boten <alex@boten.ca>

--- python-app/rolldice-server.py
+++ python-app/rolldice-server.py
@@ -3,11 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/rolldice")
 def roll_dice():
     return str(roll())
 
+
 def roll():
     return randint(1, 6)
 
-app.run()
+
+app.run(host="0.0.0.0")
